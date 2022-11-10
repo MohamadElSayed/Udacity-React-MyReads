92ada3ff84adccf1c423f510559ff032376a7936
Mohamad El-Sayed <mohamad.el-sayed@hammertech.com>
2022-11-11 00:26:55 +1100

    App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,43 @@ import "./App.css";
 import BookCase from "./Components/BookCase";
 import { Route, Routes } from "react-router-dom";
 import BookSearch from "./Components/BookSearch/BookSearch";
+import { useEffect, useState } from "react";
+import * as BooksAPI from "./api/BooksAPI";
 
 const App = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const getAllBooks = async () => {
+      const res = await BooksAPI.getAll();
+      setBooks(res);
+    };
+    getAllBooks();
+  }, []);
+
+  const handleBookShelfChange = (book) => {
+    let restBooks = books.filter((b) => b.id !== book.id);
+    setBooks([...restBooks, book]);
+  };
+
   return (
     <Routes>
-      <Route exact path="/" element={<BookCase />} />
-      <Route path="/search" element={<BookSearch />} />
+      <Route
+        exact
+        path="/"
+        element={
+          <BookCase books={books} onBookShelfChange={handleBookShelfChange} />
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <BookSearch
+            bookCase={books}
+            onBookShelfChange={handleBookShelfChange}
+          />
+        }
+      />
     </Routes>
   );
 };
